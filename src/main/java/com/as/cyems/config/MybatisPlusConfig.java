476c13902f3c281ea9495b9d5baacccca87d8527
glzx_sunshanghai <glzx_sunshanghai@devops.cp>
2018-12-07 15:56:59 +0800

    use spring 2.x

--- src/main/java/com/as/cyems/config/MybatisPlusConfig.java
+++ src/main/java/com/as/cyems/config/MybatisPlusConfig.java
@@ -28,11 +28,11 @@ public class MybatisPlusConfig {
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-        // 测试多租户】 SQL 解析处理拦截器<br>
+        // 【测试多租户】 SQL 解析处理拦截器<br>
         // 这里固定写成住户 1 实际情况你可以从cookie读取，因此数据看不到 【 麻花藤 】 这条记录（ 注意观察 SQL ）<br>
+        /*
         List<ISqlParser> sqlParserList = new ArrayList<>();
         TenantSqlParser tenantSqlParser = new TenantSqlParser();
-        /*
         tenantSqlParser.setTenantHandler(new TenantHandler() {
             @Override
             public Expression getTenantId() {
@@ -53,10 +53,9 @@ public class MybatisPlusConfig {
                 return false;
             }
         });
-        */
-
         sqlParserList.add(tenantSqlParser);
         paginationInterceptor.setSqlParserList(sqlParserList);
+        */
 //        paginationInterceptor.setSqlParserFilter(new ISqlParserFilter() {
 //            @Override
 //            public boolean doFilter(MetaObject metaObject) {
