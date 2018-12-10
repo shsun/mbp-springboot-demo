624679235b8419f57a8359c40ebed3384333c01a
glzx_sunshanghai <glzx_sunshanghai@devops.cp>
2018-12-10 19:37:13 +0800

    security

--- backend/web/src/main/java/com/as/cyems/Swagger2.java
+++ backend/web/src/main/java/com/as/cyems/Swagger2.java
@@ -2,14 +2,10 @@ package com.as.cyems;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,28 +13,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Configuration
 public class Swagger2 {
-    /*
-    @Bean
-    public Docket createRestApi() {
-        ParameterBuilder tokenPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<Parameter>();
-        tokenPar.name("token").description("令牌").modelRef(new ModelRef("string")).parameterType("query").required(false).build();
-        pars.add(tokenPar.build());
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.basePackage("com.baomidou"))
-                .paths(PathSelectors.any()).build().globalOperationParameters(pars);
-    }
-    */
 
     @Bean
-    public Docket createRestApi() {
+    public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.as"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
     }
-
 
     @SuppressWarnings("deprecation")
     private ApiInfo apiInfo() {
